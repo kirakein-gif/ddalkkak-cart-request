47b0ec198326cd6382104845e8026f7f76868ff4
kirakein-gif <kirakein@gmail.com>
2026-09-21 10:52:40 +0900

    Rename result heading to 적요 and remove duplicate title

--- cart-extract.js
+++ cart-extract.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var EXTRACTOR_VERSION = '3.0.5';
+  var EXTRACTOR_VERSION = '3.0.6';
   var scriptUrl = (document.currentScript && document.currentScript.src) || '';
   var appUrl = 'https://kirakein-gif.github.io/ddalkkak-cart-request/';
   try {
