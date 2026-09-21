27cd1b1a57e39a0630b3db50849b2cc1e98f9445
kirakein-gif <kirakein@gmail.com>
2026-09-21 11:07:08 +0900

    Shorten bookmarklet label

--- cart-extract.js
+++ cart-extract.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var EXTRACTOR_VERSION = '3.0.7';
+  var EXTRACTOR_VERSION = '3.0.8';
   var scriptUrl = (document.currentScript && document.currentScript.src) || '';
   var appUrl = 'https://kirakein-gif.github.io/ddalkkak-cart-request/';
   try {
